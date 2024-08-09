fed94dce37867321c4fd247d8fbd7b03f7c54a66
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-08-09 07:05:22 +0200

    update

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 sudo cp -r nvim /etc/nixos
 sudo cp ./*.nix /etc/nixos
 sudo cp -r hyprland /etc/nixos
-sudo nixos-rebuild switch --flake /etc/nixos#default 
+sudo nixos-rebuild switch --flake /etc/nixos#default --upgrade
